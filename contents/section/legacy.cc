6c8203f28990363fc0bdb125f13138b2d5f7d544
vichang <vichang@google.com>
2020-05-05 11:11:30 +0000

    Revert "Make com_android_i18n namespace visible"
    Revert submission 1299494-i18nApex
    
    Reason for revert: Breaking aosp_x86-eng on aosp-master
    Reverted Changes:
    I30fc3735b:Move ICU from ART APEX to i18n APEX
    Icb7e98b5c:Calling @IntraCoreApi from core-icu4j should not c...
    Ic7de63fe3:Move core-icu4j into I18n APEX
    I65b97bdba:Make com_android_i18n namespace visible
    Ia4c83bc15:Move v8 and libpac into i18n APEX
    I10e6d4948:Move core-icu4j into i18n APEX
    I8d989cad7:Move ICU from ART APEX into i18n APEX
    I72216ca12:Move ICU into i18n APEX
    Ief9dace85:Add shared library into i18n APEX and add the requ...
    I7d97a10ba:Move libpac into i18n APEX
    I90fff9c55:Move ICU from ART APEX into i18n APEX
    
    Change-Id: I8e170a25fb334c12af5c2fe4a1fb4afafd56d096

--- contents/section/legacy.cc
+++ contents/section/legacy.cc
@@ -35,7 +35,6 @@ Section BuildLegacySection(Context& ctx) {
 
   std::set<std::string> visible_apexes{
       "com.android.art",
-      "com.android.i18n",
       "com.android.neuralnetworks",
       "com.android.runtime",
       "com.android.media",
